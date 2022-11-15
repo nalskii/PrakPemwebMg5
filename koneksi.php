c8c03e1b97760346145ea2e7f6d0877bdfcad844
nalskii <103160634+nalskii@users.noreply.github.com>
2022-11-15 17:33:23 +0700

    lat4

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "127.0.0.1";
+$user = "root";
+$password = "";
+$dbName = "belajar_ajax";
+$connection = mysqli_connect($host, $user, $password, $dbName);
+if(!$connection){
+    die("Koneksi gagal : ".mysqli_connect_error());
+}
+?>
