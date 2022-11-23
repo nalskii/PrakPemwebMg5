b02bc87810bcca5da3c9a1eecec8b33b7580de9b
nalskii <103160634+nalskii@users.noreply.github.com>
2022-11-23 23:10:12 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
     $host = "127.0.0.1";
     $user = "root";
     $password = "";
-    $dbName = "tugas_ajax";
+    $dbName = "tgs5";
     $connection = mysqli_connect($host, $user, $password, $dbName);
     if(!$connection){
         die("Koneksi gagal : ".mysqli_connect_error());
